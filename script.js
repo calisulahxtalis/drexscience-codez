b75aa44a944b89b0daa9f995e0a92ade61b1d2c6
Kyz sulahcx <sulah@xifair.com>
2021-12-02 15:13:17 +0300

    code commit drexscience website

---
+++ script.js
@@ -0,0 +1,12 @@
+function changeto() {
+    // document.querySelector(".circle")
+    document.getElementById("Bcircle").style = "border-radius: 2%;"
+    document.getElementById("Bcircle").innerHTML = "<hi><u>Hello Elinah Papet mihatuu</u></h1>"
+}
+function light() {
+    document.getElementById("switch").src = "./bulbon.gif";
+    
+}
+function light2() {
+    document.getElementById("switch").src = "./bulboff.gif";
+}
